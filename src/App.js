b74ef24fb80d7a54708b1eb5974eb8553ff9af24
HenryDong <943082205@qq.com>
2020-02-13 22:21:57 +0800

    test useEffect's callBack add sec arg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-
 function App() {
     const [params, setCount] = useState({count: 0})
     const [size, setSize] = useState({
@@ -15,6 +14,9 @@ function App() {
         })
     }
     useEffect(() => {
+        console.log('Count:' + params.count)
+    }, [params.count])
+    useEffect(() => {
         document.title = params.count
     })
     useEffect(() => {
@@ -22,11 +24,31 @@ function App() {
         return () => {
             window.removeEventListener('resize', onResize, false)
         }
-    },[])
+    }, [])
+    const onClick = () => {
+        console.log('click')
+    }
+    useEffect(()=>{
+        document.querySelector('#size').addEventListener('click', onClick,false)
+        return () => {
+            document.querySelector('#size').removeEventListener('click', onClick,false)
+
+        }
+    })
     return (
-        <button type="button" onClick={() => {
-            setCount({count: params.count + 1})
-        }}>点我({params.count})({size.width}x{size.height})</button>
+        <div>
+            <button type="button" onClick={() => {
+                setCount({count: params.count + 1})
+            }}>点我({params.count})
+            </button>
+            {
+                params.count%2
+                ?<span id={'size'}>({size.width}x{size.height})</span>
+                :<p id={'size'}>奥利给</p>
+            }
+
+        </div>
+
     );
 
 }
