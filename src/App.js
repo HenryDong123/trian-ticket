116255174b796fb652ec997a84f79b0383c7be90
HenryDong <943082205@qq.com>
2020-02-13 22:06:53 +0800

    test useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [params, setCount] = useState({count: 0})
+    const [size, setSize] = useState({
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+    })
+    const onResize = () => {
+        setSize({
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        })
+    }
+    useEffect(() => {
+        document.title = params.count
+    })
+    useEffect(() => {
+        window.addEventListener('resize', onResize, false)
+        return () => {
+            window.removeEventListener('resize', onResize, false)
+        }
+    },[])
+    return (
+        <button type="button" onClick={() => {
+            setCount({count: params.count + 1})
+        }}>点我({params.count})({size.width}x{size.height})</button>
+    );
+
 }
 
 export default App;
