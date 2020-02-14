a0f896ad61aa88596d6a13d8e1ff58e7518dab80
HenryDong <943082205@qq.com>
2020-02-14 21:37:59 +0800

    test customHooks and useRef

--- src/App.js
+++ src/App.js
@@ -1,106 +1,70 @@
-import React, {Component, useState, useEffect, createContext, useContext, useMemo} from 'react';
+import React, {
+    useState,
+    useEffect,
+    useRef, useCallback,
+} from 'react';
 import './App.css';
-
-const CountContext = createContext()
-
-class Foo extends Component {
-    render() {
-        return (
-            <CountContext.Consumer>
-                {
-                    count => <h1>Foo: {count}</h1>
-                }
-            </CountContext.Consumer>
-        )
-    }
-}
-
-function Counter() {
-    const count = useContext(CountContext)
+function Counter(props) {
     return (
-        <h1>Counter: {count}</h1>
+        <h1>Count: {props.count}</h1>
     )
 }
-
-function TestMemo(props) {
+function useCounter(count) {
     return (
-        <h1>TestMemo: {props.count}</h1>
+        <h1>useCounter: {count}</h1>
     )
 }
-
-class Bar extends Component {
-    static contextType = CountContext
-
-    render() {
-        const count = this.context
-        return (
-            <h1>Bar: {count}</h1>
-
-        )
-    }
-}
-
-function App() {
-    const [params, setCount] = useState({count: 0})
-    const [size, setSize] = useState({
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight
+function useCount(defaultCount) {
+    const [count, setCount] = useState(0)
+    const it = useRef()
+    useEffect(() => {
+        it.current = setInterval(() => {
+            setCount(count => count + 1)
+        }, 1000)
+    }, [])
+    useEffect(() => {
+        if (count >= 10) {
+            clearInterval(it.current)
+        }
     })
-    const onResize = () => {
+    return [count, setCount]
+}
+function useSize() {
+    const [size,setSize] = useState(
+        {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+        }
+    )
+    const onResize = useCallback(()=>{
         setSize({
             width: document.documentElement.clientWidth,
-            height: document.documentElement.clientHeight
+            height: document.documentElement.clientHeight,
         })
-    }
-    useEffect(() => {
-        console.log('Count:' + params.count)
-    }, [params.count])
-    useEffect(() => {
-        document.title = params.count
-    })
-    useEffect(() => {
-        window.addEventListener('resize', onResize, false)
-        return () => {
-            window.removeEventListener('resize', onResize, false)
-        }
     }, [])
-    const onClick = () => {
-        console.log('click')
-    }
-    // 渲染后
-    useEffect(() => {
-        document.querySelector('#size').addEventListener('click', onClick, false)
-        return () => {
-            document.querySelector('#size').removeEventListener('click', onClick, false)
+    useEffect(()=>{
+        window.addEventListener('resize', onResize, false)
+        return ()=> {
+            window.removeEventListener('resize', onResize, false)
 
         }
-    })
-    // 渲染期间
-    const double = useMemo(() => {
-        return params.count * 2
-    }, [params.count])
-    const half = useMemo(()=>{
-        return double / 4
-    },[double])
+    },[onResize])
+    return size
+
+}
+function App() {
+    const [count, setCount] = useCount()
+    const TestUse = useCounter(count)
+    const size = useSize()
     return (
         <div>
             <button type="button" onClick={() => {
-                setCount({count: params.count + 1})
-            }}>点我({params.count})
+                setCount(count + 1)
+            }}>点我({count})
             </button>
-            {
-                params.count % 2
-                    ? <span id={'size'}>({size.width}x{size.height})</span>
-                    : <p id={'size'}>奥利给</p>
-            }
-            <p>double: {double}</p>
-            <TestMemo count={params.count}/>
-            <CountContext.Provider value={params.count}>
-                <Foo/>
-                <Bar/>
-                <Counter/>
-            </CountContext.Provider>
-
+            <Counter count={count}/>
+            {TestUse}
+            <h2>{size.width}x{size.height}</h2>
         </div>
 
     );
